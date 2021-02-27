e368fbdefb7ab4f8fe6d9c26164caf7c7bf3f322
Insan <insanboss@gmail.com>
2021-02-27 18:53:21 +0600

    Создали просмотр всех задач

--- source/To_do_list/views.py
+++ source/To_do_list/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from To_do_list.models import Task
 # Create your views here.
+
+
+def index_view(request):
+    tasks = Task.objects.all()
+    context = {
+        'tasks': tasks
+    }
+    return render(request, 'index.html', context)
+
+def task_view(request):
+    task_id = request.GET.get('pk')
+    task = Task.objects.get(pk=task_id)
+    context = {'task': task}
+    return render(request, 'task_view.html', context)
