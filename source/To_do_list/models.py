45295ae9ecea832541f30517be025d66eee9cabe
Insan <insanboss@gmail.com>
2021-02-27 18:39:03 +0600

    Создали учетную запись, настроили вывод модели Task в админке

--- source/To_do_list/models.py
+++ source/To_do_list/models.py
@@ -6,9 +6,9 @@ status_choices = [('new', 'Новая'), ('in_progress', 'В процессе'),
 
 
 class Task(models.Model):
-    title = models.CharField(max_length=250, null=True, blank=True, verbose_name='Описание')
-    status = models.CharField(max_length=50, null=True, blank=True, choices=status_choices, default='new', verbose_name='Status')
-    date = models.DateField(null=False, blank=False, verbose_name='Дата')
+    title = models.CharField(max_length=250, null=False, blank=False, verbose_name='Описание')
+    status = models.CharField(max_length=50, null=False, blank=False, choices=status_choices, default='new', verbose_name='Status')
+    date = models.DateField(null=True, blank=True, verbose_name='Дата')
 
     def __str__(self):
         return "{}. {}".format(self.pk, self.title)
