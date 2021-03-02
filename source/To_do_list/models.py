490e7010b9fb112c6b0f59b91a975a55356918ea
Insan <insanboss@gmail.com>
2021-03-02 18:03:47 +0600

    Создали поле textfield в models Сделали миграцию, применили Создали пару задач в админке и обновили фикстуры

--- source/To_do_list/models.py
+++ source/To_do_list/models.py
@@ -9,8 +9,7 @@ class Task(models.Model):
     title = models.CharField(max_length=250, null=False, blank=False, verbose_name='Описание')
     status = models.CharField(max_length=50, null=False, blank=False, choices=status_choices, default='new', verbose_name='Status')
     date = models.DateField(null=True, blank=True, verbose_name='Дата')
-    # add_info = models.TextField(max_length=1000, null=False, blank=False, verbose_name='Дополнительная информация')
-
+    add_info = models.TextField(max_length=1000, null=True, blank=True, verbose_name='Дополнительная информация')
 
     def __str__(self):
         return "{}. {}".format(self.pk, self.title)
